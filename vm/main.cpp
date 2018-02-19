63c1300de461761a9996e6424372707f1fa6ce72
Dreistein <stefan.steininger1@gmx.at>
2018-02-19 01:05:52 +0100

    Bootloader and static linking option for standalone execution

--- vm/main.cpp
+++ vm/main.cpp
@@ -1,17 +1,51 @@
 #include <iostream>
+#include <fstream>
 
 #include "VM.h"
 
-int main() {
-    std::vector<word_t> prog;
-    prog.push_back(0b0001010000000101);
-    prog.push_back(0b0001010000000101);
-    prog.push_back(0b0010100000000101);
-    prog.push_back(0b0001110000000101);
-    VM vm(prog);
-    //vm.step();
-    //vm.step();
-    //vm.step();
+std::vector<word_t> loadProgram(const std::string& filename) {
+    std::vector<word_t> program;
+    std::fstream fs;
+    fs.open(filename, std::fstream::in);
+
+    while (!fs.eof())
+    {
+        word_t word;
+        std::string line;
+        std::getline(fs, line);
+        if(line.substr(0,2) == "0b") {
+            word = static_cast<word_t>(std::stoi(line.substr(2),0,2));
+        }
+        else if (line.substr(0,2) == "0x") {
+            word = static_cast<word_t>(std::stoi(line.substr(2),0,16));
+        } else {
+            word = static_cast<word_t>(std::stoi(line));
+        }
+        program.push_back(word);
+    }
+
+    fs.close();
+    return program;
+}
+
+int main(int argc, const char *argv[]) {
+
+    if (argc != 2) {
+        std::cout << "Usage: vm <program>" << std::endl;
+        return 0;
+    }
+
+    auto program = loadProgram(argv[1]);
+
+    VM vm(program);
+
+    vm.registerInput( []() { word_t input; std::cin >> input; return input; });
+    vm.registerOutput( [](word_t out) {
+        std::cout << out << std::endl;
+    });
+
     vm.run();
+
+    std::system("pause");
     return 0;
 }
