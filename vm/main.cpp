55b64b3ce78b067ab848fc8239e312fecbdd8ece
Dreistein <1373211+Dreistein@users.noreply.github.com>
2018-03-02 15:36:54 +0100

    moved stdin io to files to make room for debugger output #1
    IN operations receive the numbers now from a file called "stdin.txt"
    OUT operations send the numbers now to a file called "stdout.txt"
    
    Additionally port 1 now reads/writes numbers as ASCII characters

--- vm/main.cpp
+++ vm/main.cpp
@@ -10,37 +10,32 @@
 
 using ToyVM::word_t;
 
-std::vector<word_t> loadProgram(const std::string& filename) {
+inline word_t str_to_i(const std::string& line) {
+    if (line.substr(0, 2) == "0b")
+        return static_cast<word_t>(std::stoi(line.substr(2), nullptr, 2));
+    else
+        return static_cast<word_t>(std::stoi(line, nullptr, 0));
+}
 
-    std::vector<word_t> program;
+inline std::vector<word_t> readFile(const std::string& filename) {
+    std::vector<word_t> values;
     std::fstream fs;
     fs.open(filename, std::fstream::in);
+    if(fs.good()) {
+        while (!fs.eof()) {
+            std::string line;
+            std::getline(fs, line);
 
-    while (!fs.eof())
-    {
-        word_t word;
-        std::string line;
-        std::getline(fs, line);
+            std::remove_if(line.begin(), line.end(), isspace);
 
-        std::remove_if(line.begin(), line.end(), isspace);
+            if (line.length() == 0)
+                continue;
 
-        if(line.length() == 0)
-            continue;
-
-        if(line.substr(0,2) == "0b") { // binary representation
-            word = static_cast<word_t>(std::stoi(line.substr(2),0,2));
+            values.push_back(str_to_i(line));
         }
-        else if (line.substr(0,2) == "0x") { // hex representation
-            word = static_cast<word_t>(std::stoi(line.substr(2),0,16));
-        }
-        else { // standard integer
-            word = static_cast<word_t>(std::stoi(line));
-        }
-        program.push_back(word);
     }
-
     fs.close();
-    return program;
+    return values;
 }
 
 int main(int argc, const char *argv[]) {
@@ -50,18 +45,42 @@ int main(int argc, const char *argv[]) {
         return 0;
     }
 
-    auto program = loadProgram(argv[1]);
+    std::cout << "Started VM\nLoading program from " << argv[1] << std::endl;
+    ToyVM::VM vm(readFile(argv[1]));
 
-    ToyVM::VM vm(program);
 
-    // register default lambdas for input / output oeration (port 0)
-    vm.registerInput( []() { word_t input; std::cin >> input; return input; });
-    vm.registerOutput( [](word_t out) {
-        std::cout << out << std::endl;
+    auto stdinBuf = readFile("stdin.txt");
+    // register default lambdas for input / output operation (port 0)
+    vm.registerInput( [&stdinBuf]() {
+        word_t word = 0;
+        if(!stdinBuf.empty()) {
+            word = stdinBuf.back();
+            stdinBuf.pop_back();
+        }
+        return word;
+    });
+    std::fstream stdoutFile;
+    stdoutFile.open("stdout.txt", std::fstream::out);
+    vm.registerOutput( [&stdoutFile](word_t out) {
+        stdoutFile << out << ' ';
+    });
+    // register character io (port 1)
+    vm.registerInput( [&stdinBuf]() {
+        word_t word = 0;
+        if(!stdinBuf.empty()) {
+            word = stdinBuf.back();
+            stdinBuf.pop_back();
+        }
+        return word;
+    });
+    vm.registerOutput( [&stdoutFile](word_t out) {
+        stdoutFile << static_cast<char>(out);
     });
 
     vm.run();
 
+    // close output stream
+    stdoutFile.close();
     // wait to show output before exiting
     std::system("pause");
     return 0;
